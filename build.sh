52373ae435b90bfcc68fea5f45329b4c2a06f76d
Paul Waring <paul@xk7.net>
2016-05-13 16:03:06 +0100

    Build and config scripts (copied from other repo)

---
+++ build.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+set -u
+set -e
+
+source ./config.sh
+
+export FULL_REBUILD=${FULL_REBUILD:-}
+
+if [ ! -z ${FULL_REBUILD} ]; then
+  echo "You are about to rebuild the entire toolchain from scratch"
+  echo "This will remove any changes you have made to the source files"
+  echo "and take substantially longer than a partial rebuild"
+  read -p "Are you sure this is what you want? (y/n) " confirm
+  case $confirm in
+    [Nn] ) exit;;
+  esac
+fi
+
+if [ ! -z ${FULL_REBUILD} ]; then
+  # Remove and then recreate prefix directory
+  if [ -d ${XC_PREFIX} ]; then
+    rm -rf ${XC_PREFIX}
+  fi
+
+  mkdir ${XC_PREFIX}
+fi
+
+# Create temp directory if it does not already exist
+if [ ! -d ${XC_TMP_DIR} ]; then
+  mkdir -p ${XC_TMP_DIR}
+fi
+
+# Download all tarballs
+if [ ! -x ${FULL_REBUILD} ]; then
+  # Binutils
+  if [ ! -f ${BINUTILS_TARBALL} ]; then
+    wget ${BINUTILS_URL} -O ${BINUTILS_TARBALL}
+  fi
+
+  # Linux
+  if [ ! -f ${KERNEL_TARBALL} ]; then
+    wget ${KERNEL_URL} -O ${KERNEL_TARBALL}
+  fi
+
+  # Glibc
+  if [ ! -f ${GLIBC_TARBALL} ]; then
+    wget ${GLIBC_URL} -O ${GLIBC_TARBALL}
+  fi
+
+  # GCC
+  if [ ! -f ${GCC_TARBALL} ]; then
+    wget ${GCC_URL} -O ${GCC_TARBALL}
+  fi
+
+  # MPFR
+  if [ ! -f ${MPFR_TARBALL} ]; then
+    wget ${MPFR_URL} -O ${MPFR_TARBALL}
+  fi
+
+  # MPC
+  if [ ! -f ${MPC_TARBALL} ]; then
+    wget ${MPC_URL} -O ${MPC_TARBALL}
+  fi
+
+  # GMP
+  if [ ! -f ${GMP_TARBALL} ]; then
+    wget ${GMP_URL} -O ${GMP_TARBALL}
+  fi
+
+  # ISL
+  if [ ! -f ${ISL_TARBALL} ]; then
+    wget ${ISL_URL} -O ${ISL_TARBALL}
+  fi
+
+  # Cloog
+  if [ ! -f ${CLOOG_TARBALL} ]; then
+    wget ${CLOOG_URL} -O ${CLOOG_TARBALL}
+  fi
+fi
+
+# Extract all tarballs
+if [ ! -z ${FULL_REBUILD} ]; then
+  # Binutils
+  if [ -d ${BINUTILS_SRC_DIR} ]; then
+    rm -rf ${BINUTILS_SRC_DIR}
+  fi
+
+  tar -xf ${BINUTILS_TARBALL} -C ${XC_TMP_DIR}
+
+  # Linux
+  if [ -d ${KERNEL_SRC_DIR} ]; then
+    rm -rf ${KERNEL_SRC_DIR}
+  fi
+
+  tar -xf ${KERNEL_TARBALL} -C ${XC_TMP_DIR}
+
+  # glibc
+  if [ -d ${GLIBC_SRC_DIR} ]; then
+    rm -rf ${GLIBC_SRC_DIR}
+  fi
+
+  tar -xf ${GLIBC_TARBALL} -C ${XC_TMP_DIR}
+
+  # GCC
+  if [ -d ${GCC_SRC_DIR} ]; then
+    rm -rf ${GCC_SRC_DIR}
+  fi
+
+  tar -xf ${GCC_TARBALL} -C ${XC_TMP_DIR}
+
+  # MPFR
+  if [ -d ${MPFR_SRC_DIR} ]; then
+    rm -rf ${MPFR_SRC_DIR}
+  fi
+
+  tar -xf ${MPFR_TARBALL} -C ${XC_TMP_DIR}
+  ln -s ${MPFR_SRC_DIR} ${GCC_SRC_DIR}/mpfr
+
+  # MPC
+  if [ -d ${MPC_SRC_DIR} ]; then
+    rm -rf ${MPC_SRC_DIR}
+  fi
+
+  tar -xf ${MPC_TARBALL} -C ${XC_TMP_DIR}
+  ln -s ${MPC_SRC_DIR} ${GCC_SRC_DIR}/mpc
+
+  # GMP
+  if [ -d ${GMP_SRC_DIR} ]; then
+    rm -rf ${GMP_SRC_DIR}
+  fi
+
+  tar -xf ${GMP_TARBALL} -C ${XC_TMP_DIR}
+  ln -s ${GMP_SRC_DIR} ${GCC_SRC_DIR}/gmp
+
+  # ISL
+  if [ -d ${ISL_SRC_DIR} ]; then
+    rm -rf ${ISL_SRC_DIR}
+  fi
+
+  tar -xf ${ISL_TARBALL} -C ${XC_TMP_DIR}
+  ln -s ${ISL_SRC_DIR} ${GCC_SRC_DIR}/isl
+
+  # Cloog
+  if [ -d ${CLOOG_SRC_DIR} ]; then
+    rm -rf ${CLOOG_SRC_DIR}
+  fi
+
+  tar -xf ${CLOOG_TARBALL} -C ${XC_TMP_DIR}
+  ln -s ${CLOOG_SRC_DIR} ${GCC_SRC_DIR}/cloog
+fi
+
+# Build binutils
+# Remove and recreate the build directory
+if [ -d ${BINUTILS_BUILD_DIR} ]; then
+  rm -rf ${BINUTILS_BUILD_DIR}
+fi
+
+mkdir ${BINUTILS_BUILD_DIR}
+
+cd ${BINUTILS_BUILD_DIR}
+${BINUTILS_SRC_DIR}/configure ${BINUTILS_CONFIGURE_OPTIONS[*]}
+make
+make install
+
+# Build kernel headers (no need for separate build directory)
+cd ${KERNEL_SRC_DIR}
+make ${KERNEL_MAKE_OPTIONS[*]}
+
+# Build GCC (first pass)
+# Remove and recreate the build directory
+if [ -d ${GCC_BUILD_DIR} ]; then
+  rm -rf ${GCC_BUILD_DIR}
+fi
+
+mkdir ${GCC_BUILD_DIR}
+
+cd ${GCC_BUILD_DIR}
+${GCC_SRC_DIR}/configure ${GCC_CONFIGURE_OPTIONS[*]}
+make all-gcc
+make install-gcc
+
+# Build glibc (first pass)
+# Remove and recreate the build directory
+if [ -d ${GLIBC_BUILD_DIR} ]; then
+  rm -rf ${GLIBC_BUILD_DIR}
+fi
+
+mkdir ${GLIBC_BUILD_DIR}
+
+cd ${GLIBC_BUILD_DIR}
+${GLIBC_SRC_DIR}/configure ${GLIBC_CONFIGURE_OPTIONS[*]}
+make install-bootstrap-headers=yes install-headers
+make csu/subdir_lib
+install csu/crt1.o csu/crti.o csu/crtn.o ${XC_HEADER_DIR}/lib
+${XC_TARGET}-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o ${XC_HEADER_DIR}/lib/libc.so
+touch ${XC_HEADER_DIR}/include/gnu/stubs.h
+
+
+# Build GCC (second pass)
+cd ${GCC_BUILD_DIR}
+make all-target-libgcc
+make install-target-libgcc
+
+# Build glibc (second pass)
+cd ${GLIBC_BUILD_DIR}
+make
+make install
+
+# Build and install GCC (third pass)
+cd ${GCC_BUILD_DIR}
+make all
+make install
---
+++ config.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# Target architecture for the toolchain. Depending on the architecture, you
+# may need all parts of the triplet (e.g. for a Raspberry Pi you should set
+# XC_TARGET to "arm-unknown-linux-gnueabihf").
+export XC_TARGET="aarch64-linux-gnu"
+
+# Kernel target architecture. The kernel has slightly different names for its
+# targets, e.g. for a RPi you shoud set this to "arm"
+export XC_KERNEL_TARGET="arm64"
+
+# Prefix to use for downloading and building the toolchain, as well as the
+# destination directory for the final binaries.
+# This prefix must not contain spaces, otherwise the build will fail (this is
+# due to the binutils makefiles and nothing to do with this build script).
+# Do not specify an existing directory for the prefix as it will be deleted
+# each time you do a full rebuild.
+export XC_PREFIX="${PWD}/xc-${XC_TARGET}"
+
+# Don't edit these configuration variables.
+export XC_TMP_DIR="${XC_PREFIX}/${XC_TARGET}/tmp"
+export XC_HEADER_DIR="${XC_PREFIX}/${XC_TARGET}"
+export PATH="${XC_PREFIX}/bin:${PATH}"
+
+# GNU (binutils, GCC + dependencies) and Sourceware (newlib) URLs. You can
+# change these to a local mirror if required.
+export GNU_BASE_URL="http://ftpmirror.gnu.org"
+export SOURCEWARE_BASE_URL="ftp://sourceware.org/pub"
+export KERNEL_BASE_URL="https://www.kernel.org/pub/linux/kernel"
+export GCC_BASE_URL="ftp://gcc.gnu.org/pub/gcc"
+
+# Version numbers for the relevant components of the toolchain. Not all
+# version combinations have been tested. Unless you need a feature in
+# a later version, do not edit these options.
+export BINUTILS_VERSION="2.26"
+
+# Kernel headers are required for glibc
+# Generally it is best to choose the latest stable release
+export KERNEL_VERSION_MAJOR="4"
+export KERNEL_VERSION_MINOR="5.2"
+export KERNEL_VERSION="${KERNEL_VERSION_MAJOR}.${KERNEL_VERSION_MINOR}"
+
+export GLIBC_VERSION="2.23"
+
+# The GCC version number should be in the same minor series as your host
+# compiler. For example, if your host has GCC 4.9.1, you can probably set
+# GCC_VERSION to the latest version of the 4.9.x series, but you should not
+# set it to 5.x.
+export GCC_VERSION="5.3.0"
+export GCC_LANGS="c,c++"
+
+export MPFR_VERSION="3.1.4"
+export MPC_VERSION="1.0.3"
+
+export GMP_VERSION="6.1.0"
+export GMP_VERSION_MINOR=""
+
+export ISL_VERSION="0.16.1"
+export CLOOG_VERSION="0.18.1"
+
+export GLOBAL_CONFIGURE_OPTIONS=(
+  "--target=${XC_TARGET}"
+  "--disable-multilib"
+  "--disable-werror"
+  "--disable-threads"
+)
+
+# You should not need to edit any of the following variables unless the build
+# process fails.
+export BINUTILS_FILENAME="binutils-${BINUTILS_VERSION}.tar.bz2"
+export BINUTILS_URL="${GNU_BASE_URL}/binutils/${BINUTILS_FILENAME}"
+export BINUTILS_TARBALL="${XC_TMP_DIR}/${BINUTILS_FILENAME}"
+export BINUTILS_SRC_DIR="${XC_TMP_DIR}/binutils-${BINUTILS_VERSION}"
+export BINUTILS_BUILD_DIR="${XC_TMP_DIR}/build-binutils"
+export BINUTILS_CONFIGURE_OPTIONS=(
+  "--prefix=${XC_PREFIX}"
+)
+
+BINUTILS_CONFIGURE_OPTIONS+=(${GLOBAL_CONFIGURE_OPTIONS[*]})
+
+export KERNEL_FILENAME="linux-${KERNEL_VERSION}.tar.xz"
+export KERNEL_URL="${KERNEL_BASE_URL}/v${KERNEL_VERSION_MAJOR}.x/${KERNEL_FILENAME}"
+export KERNEL_TARBALL="${XC_TMP_DIR}/${KERNEL_FILENAME}"
+export KERNEL_SRC_DIR="${XC_TMP_DIR}/linux-${KERNEL_VERSION}"
+export KERNEL_MAKE_OPTIONS=(
+  "ARCH=${XC_KERNEL_TARGET}"
+  "INSTALL_HDR_PATH=${XC_HEADER_DIR}"
+  "headers_install"
+)
+
+export GLIBC_FILENAME="glibc-${GLIBC_VERSION}.tar.xz"
+export GLIBC_URL="${GNU_BASE_URL}/glibc/${GLIBC_FILENAME}"
+export GLIBC_TARBALL="${XC_TMP_DIR}/${GLIBC_FILENAME}"
+export GLIBC_SRC_DIR="${XC_TMP_DIR}/glibc-${GLIBC_VERSION}"
+export GLIBC_BUILD_DIR="${XC_TMP_DIR}/build-glibc"
+export GLIBC_CONFIGURE_OPTIONS=(
+  "--prefix=${XC_HEADER_DIR}"
+  "--build=${MACHTYPE}"
+  "--host=${XC_TARGET}"
+  "--with-headers=${XC_HEADER_DIR}/include"
+  "libc_cv_forced_unwind=yes"
+)
+
+GLIBC_CONFIGURE_OPTIONS+=(${GLOBAL_CONFIGURE_OPTIONS[*]})
+
+export GCC_FILENAME="gcc-${GCC_VERSION}.tar.gz"
+export GCC_URL="${GNU_BASE_URL}/gcc/gcc-${GCC_VERSION}/${GCC_FILENAME}"
+export GCC_TARBALL="${XC_TMP_DIR}/${GCC_FILENAME}"
+export GCC_SRC_DIR="${XC_TMP_DIR}/gcc-${GCC_VERSION}"
+export GCC_BUILD_DIR="${XC_TMP_DIR}/build-gcc"
+
+export GCC_CONFIGURE_OPTIONS=(
+  "--prefix=${XC_PREFIX}"
+  "--enable-languages=${GCC_LANGS}"
+)
+
+GCC_CONFIGURE_OPTIONS+=(${GLOBAL_CONFIGURE_OPTIONS[*]})
+
+export MPFR_FILENAME="mpfr-${MPFR_VERSION}.tar.xz"
+export MPFR_URL="${GNU_BASE_URL}/mpfr/${MPFR_FILENAME}"
+export MPFR_TARBALL="${XC_TMP_DIR}/${MPFR_FILENAME}"
+export MPFR_SRC_DIR="${XC_TMP_DIR}/mpfr-${MPFR_VERSION}"
+
+export MPC_FILENAME="mpc-${MPC_VERSION}.tar.gz"
+export MPC_URL="${GNU_BASE_URL}/mpc/${MPC_FILENAME}"
+export MPC_TARBALL="${XC_TMP_DIR}/${MPC_FILENAME}"
+export MPC_SRC_DIR="${XC_TMP_DIR}/mpc-${MPC_VERSION}"
+
+export GMP_FILENAME="gmp-${GMP_VERSION}${GMP_VERSION_MINOR}.tar.xz"
+export GMP_URL="${GNU_BASE_URL}/gmp/${GMP_FILENAME}"
+export GMP_TARBALL="${XC_TMP_DIR}/${GMP_FILENAME}"
+export GMP_SRC_DIR="${XC_TMP_DIR}/gmp-${GMP_VERSION}"
+
+export ISL_FILENAME="isl-${ISL_VERSION}.tar.bz2"
+export ISL_URL="${GCC_BASE_URL}/infrastructure/${ISL_FILENAME}"
+export ISL_TARBALL="${XC_TMP_DIR}/${ISL_FILENAME}"
+export ISL_SRC_DIR="${XC_TMP_DIR}/isl-${ISL_VERSION}"
+
+export CLOOG_FILENAME="cloog-${CLOOG_VERSION}.tar.gz"
+export CLOOG_URL="${GCC_BASE_URL}/infrastructure/${CLOOG_FILENAME}"
+export CLOOG_TARBALL="${XC_TMP_DIR}/${CLOOG_FILENAME}"
+export CLOOG_SRC_DIR="${XC_TMP_DIR}/cloog-${CLOOG_VERSION}"
