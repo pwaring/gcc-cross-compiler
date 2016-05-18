ec5563f209c43006db122935b5c0b79220094490
Paul Waring <paul@xk7.net>
2016-05-18 14:56:31 +0100

    Bump kernel version to 4.6

--- config.sh
+++ config.sh
@@ -48,7 +48,7 @@ export BINUTILS_VERSION="2.26"
 # Kernel headers are required for glibc
 # Generally it is best to choose the latest stable release
 export KERNEL_VERSION_MAJOR="4"
-export KERNEL_VERSION_MINOR="5.4"
+export KERNEL_VERSION_MINOR="6"
 export KERNEL_VERSION="${KERNEL_VERSION_MAJOR}.${KERNEL_VERSION_MINOR}"
 
 export GLIBC_VERSION="2.23"
